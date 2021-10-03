f93d9f4e3b38dd7f97c758960df8325390a623ef
Ramon Adàlia <adaliaramon@gmail.com>
2021-10-03 15:43:43 +0200

    Set all random prices to 0 and increased amount of 'Rupees (200)'

--- ItemPool.py
+++ ItemPool.py
@@ -243,10 +243,7 @@ normal_rupees = (
     ['Rupees (200)'] * 3)
 
 shopsanity_rupees = (
-    ['Rupees (5)'] * 2 +
-    ['Rupees (20)'] * 10 +
-    ['Rupees (50)'] * 10 +
-    ['Rupees (200)'] * 5 +
+    ['Rupees (200)'] * 27 +
     ['Progressive Wallet'])
 
 
